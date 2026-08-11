f97efc117e2f12a974ff9c7d5a2236941010138d
pragnyasree21-iij <pragnyasree21@gmail.com>
2026-08-11 16:04:54 +0530

    Time: 303 ms (9.18%) | Memory: 81.4 MB (7.43%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -10,7 +10,7 @@ public:
                 st++;
             }
             ch.insert(s[end]);
-            l = max(l, end - st + 1);
+            l = max(l,end-st+1);
         }
         return l;
     }
